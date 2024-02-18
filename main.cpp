0359e6c2bb0651b351aad67696d213698062193a
Audrey Gorter <algorter16@gmail.com>
2024-02-17 18:29:40 -0600

    Started main outline

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "TableDrivenFSA.h"
+#include "Lexer.h"
+
+using namespace std;
+
+// construct FSA for Java 0 function
+
+// read file function
+
+
+// add args to pass program to compiler
+int main() {
+    // main shit goes here
+
+    // configure fsa for java 0
+
+    // create the lexer
+
+    // read in program file
+
+    // tokenize it
+
+    // retrieve tokens and add to token list
+
+    // add more to do symbol table, parse etc.
+}
