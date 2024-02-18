129fa4fa83627adf2de6112a90c55807c0abc9df
Audrey Gorter <algorter16@gmail.com>
2024-02-18 00:40:29 -0600

    Added a File Handler class to read various files

---
+++ FileHandler.h
@@ -0,0 +1,21 @@
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#pragma once
+
+class FileHandler
+{
+public:
+    FileHandler();
+    ~FileHandler();
+
+    // read source code file
+    // write token list file
+    // read token list file
+    // write symbol table file
+
+private:
+
+};
+
+#endif
