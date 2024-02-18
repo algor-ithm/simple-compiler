875456d8f172ecf2ce4b3c38435bc053f1cf1407
Audrey Gorter <algorter16@gmail.com>
2024-02-17 18:03:57 -0600

    Declared functions for lexer

--- Lexer.h
+++ Lexer.h
@@ -1,15 +1,40 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include "enumDefinitions.h"
+#include "TableDrivenFSA.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Define the token Struct
+struct Token {
+    string lexeme;
+    string type;
+
+    // Token constructor
+    Token(const string& lexeme, const string& type);
+};
+
 class Lexer
 {
 private:
-
+    // variables
+    TableDrivenFSA fsa;
+    string input;
+    size_t position = 0;
+    vector<Token> tokens;
+    // functions
+    InputType charToInputType(char ch);
+    void addTokens(const string& lexeme, const string& type);
+    string mapStateToTokenType(State state) const;
 
 public:
-    Lexer();
-   
-
+    explicit Lexer(const TableDrivenFSA& fsa);
+    void setInput(const string& newInput);
+    void tokenize();
+    const vector<Token>& getTokens() const;
 };
 
 #endif
