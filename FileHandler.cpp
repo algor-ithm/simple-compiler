51a3490dcb15e95788759d6ba36af1fa9ff5edfc
Audrey Gorter <algorter16@gmail.com>
2024-02-23 14:37:51 -0600

    Implement read program function

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -1,11 +1,11 @@
 #include "FileHandler.h"
 
-FileHandler::FileHandler()
-{
-
-}
-
-FileHandler::~FileHandler()
-{
-
+string FileHandler::readProgram(const string &filePath) const {
+    ifstream pgmFile(this->filePath);
+    if (!pgmFile.is_open()) {
+        throw runtime_error("Could not open file: " + this->filePath);
+    }
+    string buffer((std::istreambuf_iterator<char>(pgmFile)),
+                         std::istreambuf_iterator<char>());
+    return buffer;
 }
--- FileHandler.h
+++ FileHandler.h
@@ -1,19 +1,28 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
-#pragma once
+#include "Lexer.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+using namespace std; 
 
 class FileHandler
 {
+private:
+    string filePath;
+
 public:
-    FileHandler();
-    ~FileHandler();
-
-    // read source code file
-    // write token list file
+    string readProgram(const string &filePath) const;
+    // write token list to file
+    void writeTokenList(const vector<Token>& tokens);
     // read token list file
+    void readTokenList();
     // write symbol table file
-
+    void writeSymbolTable();
 };
 
 #endif
