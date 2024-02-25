6e2c4902f1c484f99d3083aaad9af0444dccb45f
Audrey Gorter <algorter16@gmail.com>
2024-02-24 22:29:08 -0600

    Finished implemented program reading and token list writing

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -1,11 +1,28 @@
 #include "FileHandler.h"
 
 string FileHandler::readProgram(const string &filePath) const {
-    ifstream pgmFile(this->filePath);
+    ifstream pgmFile(filePath);
     if (!pgmFile.is_open()) {
         throw runtime_error("Could not open file: " + this->filePath);
     }
     string buffer((std::istreambuf_iterator<char>(pgmFile)),
                          std::istreambuf_iterator<char>());
     return buffer;
+}
+
+void FileHandler::writeTokenList(const vector<Token>& tokens) {
+    const string filename = "textFiles/token-list.tsv";
+
+    ofstream tokenFile(filename);
+    if (!tokenFile.is_open()) {
+        throw runtime_error("Couldn't not open file: " + filename);
+    }
+    // Write header line (needed?)
+    tokenFile << "Lexeme\tTokenType\n";
+
+    // Iterate through vector and write each token with its type
+    for (const auto& token : tokens){
+        tokenFile << token.lexeme << "\t" << token.type << "\n";
+    }
+    tokenFile.close();
 }
--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ using namespace std;
 
 // add args to pass program to compiler and output file names?
 int main(int argc, char* argv[]) {
+    string code;
+    vector<Token> tokenList;
+    TokenFSA fsa;
+    Lexer lexer(fsa);
+    FileHandler fHandler;
+
     // check that a program file was a command line arguement
     if (argc < 2) {
         cerr << "File path required. \n" << "Usage: " << argv[0] << " <filepath>" << endl;
@@ -16,23 +22,20 @@ int main(int argc, char* argv[]) {
     }
     // Get file path
     string pgmFile = argv[1];
-    string code;
-
-    TokenFSA fsa;
-    Lexer lexer(fsa);
-    FileHandler fHandler;
-
-    // read the program file 
-    code = fHandler.readProgram(pgmFile);
-
+    try {
+        code = fHandler.readProgram(pgmFile);
+    } catch (const std::exception& e) {
+        cerr << "Error reading file: " << e.what() << endl;
+        return 1;
+    }
     // Set the program as input for the lexer
     lexer.setInput(code);
- 
+    //Tokenize the program and retrieve token list 
+    lexer.tokenize();
+    tokenList = lexer.getTokens();
+    // Write token list to a file
+    fHandler.writeTokenList(tokenList);
 
-
-    // tokenize it
-
-    // retrieve tokens and add to token list
 
     // add more to do symbol table, parse etc.
     return 0;
